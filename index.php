2153b9d858e607794f1004cd467208ca755c7f5b
Дмитрий Мохирев <97874817+dvmohirev@users.noreply.github.com>
2022-09-06 17:23:44 +0500

    first file in project

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-echo 'Hello, PHP';
+if (isset($_GET['path'])){
+    switch ($_GET['path']){
+        case 'hello':{
+            echo 'Hello!';
+            break;
+        }
+        case 'message':{
+            echo 'message';
+            break;
+        }
+    }
+}
